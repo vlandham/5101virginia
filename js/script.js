d541b0d2bc3271e6a1cd15234cf9041e5b7e3696
Jim V <jfv@stowers.org>
2013-07-19 10:23:43 -0500

    add images

--- js/script.js
+++ js/script.js
@@ -2,6 +2,41 @@
 
 */
 
+this.imagePreview = function(){	
+	/* CONFIG */
+		
+		xOffset = 250;
+		yOffset = 30;
+
+		
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result
+		
+	/* END CONFIG */
+	$("a.preview").hover(function(e){
+    console.log('preview');
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='preview'><img src='"+ this.href +"' alt='Image preview' />"+ c +"</p>");								 
+		$("#preview")
+      .css("position", "absolute")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#preview").remove();
+    });	
+	$("a.preview").mousemove(function(e){
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
 
 
 
